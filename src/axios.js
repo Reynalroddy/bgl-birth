9e4b6842cbaa54656f457b27ddd386f977d83818
Reynalroddy <58977649+Reynalroddy@users.noreply.github.com>
2023-02-20 15:43:41 +0100

    tok

--- src/axios.js
+++ src/axios.js
@@ -10,7 +10,7 @@ const authFetch = axios.create({
 authFetch.interceptors.request.use(
   function (req) {
     // const token = localStorage.getItem("token");
-    const token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VyX0lEIjo0LCJFbWFpbCI6Imtlc3RvbkBlbWFpbC5jb20iLCJSb2xlX0lEIjpudWxsLCJpYXQiOjE2NzM2MTYwOTQsImV4cCI6MTY3MzY5ODg5NH0.XZXaaOsCeVxtw1-DmPPe9mJtkNGAz8LH5ttDqHxTLc4"
+    const token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VyX0lEIjo0LCJMR0FfSUQiOjMwOSwiUmVnX0NlbnRlcl9JRCI6MTM0MiwiVXNlck5hbWUiOiJLRVNUT04iLCJSb2xlX0lEIjpudWxsLCJFbWFpbCI6Imtlc3RvbkBlbWFpbC5jb20iLCJpYXQiOjE2NzY5MDQxOTAsImV4cCI6MTY3Njk4Njk5MH0.iVquXjxce0UPRz9J4jVIsnFjYU4HprNSlyNwSC6DX0o"
     if (token) {
       // const toks = localStorage.getItem("token");
       req.headers.authorization = `Bearer ${token}`;
