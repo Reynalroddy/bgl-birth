d5aa30c67cb14f945db2f7fa7d51d155b3803eb4
Reynalroddy <58977649+Reynalroddy@users.noreply.github.com>
2023-04-27 15:58:27 +0100

    gy

--- src/axios.js
+++ src/axios.js
@@ -10,7 +10,7 @@ const authFetch = axios.create({
 authFetch.interceptors.request.use(
   function (req) {
     // const token = localStorage.getItem("token");
-    const token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VyX0lEIjoiNCIsIkxHQV9JRCI6IjMwOSIsIlJlZ19DZW50ZXJfSUQiOiIxMzQyIiwiVXNlck5hbWUiOiJLRVNUT04iLCJSb2xlX0lEIjoiNSIsIlN0YXRlX0lEIjoiMTQiLCJFbWFpbCI6Imtlc3RvbkBlbWFpbC5jb20iLCJpYXQiOjE2ODI1MjM1NDUsImV4cCI6MTY4MjYwNjM0NX0.0T94Aj6DOxPTe3K2ZXIawxN16YODJczqMjM2MDCShhs"
+    const token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VyX0lEIjoiNCIsIkxHQV9JRCI6IjMwOSIsIlJlZ19DZW50ZXJfSUQiOiIxMzQyIiwiVXNlck5hbWUiOiJLRVNUT04iLCJSb2xlX0lEIjoiNSIsIlN0YXRlX0lEIjoiMTQiLCJFbWFpbCI6Imtlc3RvbkBlbWFpbC5jb20iLCJpYXQiOjE2ODI2MDc0NzYsImV4cCI6MTY4MjY5MDI3Nn0.V9KEt9VIWRDvcHtvIAdKyzbNdxGiTOJXNh1nXP6Llj4"
     if (token) {
       // const toks = localStorage.getItem("token");
       req.headers.authorization = `Bearer ${token}`;
